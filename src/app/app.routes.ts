d50be64b6eb7186b903f2ce789c732f882210834
Mehedi Hasan <mh.ju1803@gmail.com>
2024-01-16 10:39:39 +0600

    Booking form added

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { BookingFormComponent } from './Components/booking-form/booking-form.component';
+
+export const routes: Routes = [
+    {path: 'booking', component: BookingFormComponent}
+];
